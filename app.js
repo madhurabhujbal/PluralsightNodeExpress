9fc0fce1bc394d61c5a856652ef45ea037cef666
Madhura <madhura.bhujbal@gmail.com>
2020-04-10 18:22:49 +0100

    Render index page using ejs

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ const app = express();
 
 app.use(morgan('tiny')); // switch 'tiny'->'combined' for detailed log
 app.use(express.static(path.join(__dirname, '/public/'))); // express.static indicates that static files in given directory are to be referred
-
 /*
 similar to above, specify alternate path for finding css/js files, if not found in /public folder.
 e.g: app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
  */
 
+app.set('views', __dirname + '/views/');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/', 'index.html'));
+  res.render('index', {name: 'Madhura'});
 });
 
 app.listen(3000, () => {
