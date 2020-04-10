b77437363889545fb1a32f0501d57e0d089514a8
Madhura <madhura.bhujbal@gmail.com>
2020-04-10 17:53:14 +0100

    Fix all errors using eslint

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var express = require('express');
-var debug = require('debug')('app');
-var chalk = require('chalk');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(morgan('tiny')); // switch 'tiny'->'combined' for detailed log
-app.use(express.static(path.join(__dirname, '/public/'))) //express.static indicates that static files in given directory are to be referred
-//similar to above, specify alternate path for finding css/js files, if not found in /public folder.
-//e.g: app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use(express.static(path.join(__dirname, '/public/'))); // express.static indicates that static files in given directory are to be referred
 
-app.get('/', (req, res) =>
-{
-    res.sendFile(path.join(__dirname, 'views/', 'index.html'));
+/*
+similar to above, specify alternate path for finding css/js files, if not found in /public folder.
+e.g: app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+ */
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/', 'index.html'));
 });
 
-app.listen(3000, ()=>{
-    debug(`server listening on port ${chalk.blueBright('3000')}`);
+app.listen(3000, () => {
+  debug(`server listening on port ${chalk.blueBright('3000')}`);
 });
