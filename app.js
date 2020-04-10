20f0dee8507fa4640197a43d45eb1668c7a4559b
Madhura <madhura.bhujbal@gmail.com>
2020-04-10 18:46:41 +0100

    Add list items using ejs

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', __dirname + '/views/');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {name: 'Madhura'});
+  res.render('index', {name: 'Madhura', list: ['Apple', 'Banana']});
 });
 
 app.listen(3000, () => {
