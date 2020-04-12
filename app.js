b77908cc453be3b6b795ba2c02c288845dfbe75f
Madhura <madhura.bhujbal@gmail.com>
2020-04-12 18:05:23 +0100

    Route all requests for books seperately using bookRouter

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const bookRouter = express.Router();
 
 app.use(morgan('tiny')); // switch 'tiny'->'combined' for detailed log
 app.use(express.static(path.join(__dirname, '/public/'))); // express.static indicates that static files in given directory are to be referred
@@ -18,8 +19,19 @@ app.set('view engine', 'ejs');
 
 const books = [{title: 'A book of simple living' , author: 'Ruskin Bond'}, {title: 'Chaavaa', author: 'Shivaji Sawant'}, {title: 'Batatyachi chaal', author: 'P. L. Deshpande'}];
 
+bookRouter.route('/')
+.get((req, res) => {
+    res.render('books', {name: 'Books', nav: [{title: 'Books', link: '/books'}, {title: 'Authors', link: '/authors'}], books});
+});
+
+app.use('/books', bookRouter);
+
 app.get('/', (req, res) => {
   res.render('index', {name: 'Madhura', nav: [{title: 'Books', link: '/books'}, {title: 'Authors', link: '/authors'}], books});
+});
+
+app.get('/authors', (req, res) => {
+    res.send('Page under construction');
 });
 
 app.listen(3000, () => {
