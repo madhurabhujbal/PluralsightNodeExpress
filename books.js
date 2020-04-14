80f02d6986ade52045729eafb13c4fa60fe45f19
Madhura <madhura.bhujbal@gmail.com>
2020-04-14 22:04:55 +0100

    Move /books/... in controller

--- books.js
+++ books.js
@@ -1,46 +1,16 @@
 const express = require('express');
-const {MongoClient, ObjectID} = require('mongodb');
-const debug = require('debug')('app:books');
 
 function router(nav) {
     const bookRouter = express.Router();
-    const {getIndex} = require('./controller/booksController')(nav);
+    const {getIndex, getId, middleware} = require('./controller/booksController')(nav);
 
-    bookRouter.use((req, res, next) => {
-        if (req.user) {
-            next();
-        } else {
-            res.redirect('/');
-        }
-    });
+    bookRouter.use(middleware);
 
     bookRouter.route('/')
     .get(getIndex);
 
     bookRouter.route('/:id')
-    .get((req, res) => {
-        const {id} = req.params;
-        //connect
-        const url = 'mongodb://localhost:27017';
-        const dbName = 'LibraryApp';
-        const collectionName = 'books';
-        let conn;
-        (async function mongo(){
-            try{
-            conn = await MongoClient.connect(url);
-            //retrieve
-            const db = conn.db(dbName);
-            const booksCollection = await db.collection(collectionName);
-            const book = await booksCollection.findOne({'_id': new ObjectID(id)});
-            res.render('bookView', {name: 'Books', nav, book});
-        }
-        catch(err){
-            debug(err.stack);
-        }
-        //close
-        conn.close();
-    }());
-    });
+    .get(getId);
     return bookRouter;
 };
 
--- controller/booksController.js
+++ controller/booksController.js
@@ -24,8 +24,43 @@ function controller (nav) {
         conn.close();
     }());
     }
+
+    function getId(req, res) {
+        const {id} = req.params;
+        //connect
+        const url = 'mongodb://localhost:27017';
+        const dbName = 'LibraryApp';
+        const collectionName = 'books';
+        let conn;
+        (async function mongo(){
+            try{
+            conn = await MongoClient.connect(url);
+            //retrieve
+            const db = conn.db(dbName);
+            const booksCollection = await db.collection(collectionName);
+            const book = await booksCollection.findOne({'_id': new ObjectID(id)});
+            res.render('bookView', {name: 'Books', nav, book});
+        }
+        catch(err){
+            debug(err.stack);
+        }
+        //close
+        conn.close();
+    }());
+    }
+
+    function middleware(req, res, next) {
+        if (req.user) {
+            next();
+        } else {
+            res.redirect('/');
+        }
+    }
+
     return {
-        getIndex
+        getIndex,
+        getId,
+        middleware
     };
 }
 
