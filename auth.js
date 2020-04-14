697a87f59fbe56be51da43a4c7f35d47cb1e06ad
Madhura <madhura.bhujbal@gmail.com>
2020-04-14 13:23:03 +0100

    Add skeleton for login

--- auth.js
+++ auth.js
@@ -7,6 +7,13 @@ const authRouter = express.Router();
 authRouter.route('/signUp')
 .post((req, res) => {
     debug(req.body);
-    res.json(req.body);
+    req.login(req.body,() => {
+        res.redirect('/auth/profile');
+    });
+});
+
+authRouter.route('/profile')
+.get((req, res) => {
+    res.json(req.user);
 });
 module.exports = authRouter;
