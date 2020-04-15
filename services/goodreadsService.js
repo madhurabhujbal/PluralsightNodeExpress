de6184c51d80f89e31510fa9df8414606664f9e9
Madhura <madhura.bhujbal@gmail.com>
2020-04-15 11:44:55 +0100

    Retrieve book info using goodreads API

--- services/goodreadsService.js
+++ services/goodreadsService.js
@@ -1,7 +1,26 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
+
+const parser = xml2js.Parser({ explicitArray : false });
 function bookService() {
     function getById() {
         return new Promise((resolve, reject) => {
-            resolve({ 'description' : 'our description'});
+            axios.get('https://www.goodreads.com/book/show/656.xml?key=WaN4ff1kNJ9KovbI03jjA')
+            .then((response) => {
+                debug(response);
+                parser.parseString(response.data, (error, jsonResult) => {
+                    if(error) {
+                        reject(error);
+                    } else {
+                        resolve(jsonResult.GoodreadsResponse.book);
+                    }
+                });
+            })
+            .catch((error) => {
+                debug(error);
+                reject(error);
+            });
         });
     }
 
